619c0380c0c48df111e0ca90852d661d470d0e19
freelancernoor <info@devnurul.me>
2021-08-31 20:57:06 +0600

    diffrent between fetch and async and await

---
+++ new.js
@@ -0,0 +1,9 @@
+class Person {
+    constructor (firstName, lastName, salary){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+}
+     const heroPerson = new Person("Hero", "Balam", 25000);
+     console.log(heroPerson);
